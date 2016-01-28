9ab46580c331c4df05c4b9b17a2521faa0406014
Kellan Childers <Kellan Childers@gmail.com>
2016-01-28 13:29:07 -0500

    Elaborated on commanddict's documentation

--- controls/commanddict.py
+++ controls/commanddict.py
@@ -1,8 +1,9 @@
 import model.keyfunctions as kf
 __author__ = 'Kellan Childers'
+"""Compile all user-facing functions here with easy to remember names to allow keymapping.
+   Actual functions should be defined in model.keyfunctions.py."""
 
 
-"""Compile all user-facing functions here with easy to remember names to allow keymapping."""
 commands = {"quit": kf.test1,
             "next": kf.test2
             }
