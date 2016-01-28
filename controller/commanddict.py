fe7a08c2d8e8579aca4c889d2323c918dedec84f
Kellan Childers <Kellan Childers@gmail.com>
2016-01-28 13:45:24 -0500

    Fixed bug in model package name.

--- controller/commanddict.py
+++ controller/commanddict.py
@@ -1,7 +1,7 @@
 import model.keyfunctions as kf
 __author__ = 'Kellan Childers'
 """Compile all user-facing functions here with easy to remember names to allow keymapping.
-   Actual functions should be defined in model.keyfunctions.py."""
+   Actual functions should be defined in Modeller.keyfunctions.py."""
 
 
 commands = {"quit": kf.test1,
